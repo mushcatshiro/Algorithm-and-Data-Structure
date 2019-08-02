abb2baf911093ff294ee9fa3be789d8999dcf287
shiro <yingjieho@hotmail.com>
2019-08-02 22:10:58 +0800

    update

--- D20_solution.py
+++ D20_solution.py
@@ -15,6 +15,48 @@ class sLL:
 	def __init__(self, root):
 		self.root = root
 
-	def add_node(self, value):
-		while root.next != None:
-			self.add_node(value)
+	def add_node(self, node):
+		current = self.root
+		if (self.root):
+			while (current.next):
+				current = current.next
+			current.next = node
+		else:
+			self.root = node
+		return(self.root)
+
+	def traversal(self):
+		current = self.root
+
+		while (current):
+			print(current.value)
+			current = current.next
+
+a = [3, 7, 8, 10]
+b = [99, 1, 8, 10]
+
+for n, element in enumerate(a):
+	if (n == 0):
+		LL1 = sLL(node(element))
+	else:
+		A = LL1.add_node(node(element))
+
+for n, element in enumerate(b):
+	if (n == 0):
+		LL2 = sLL(node(element))
+	else:
+		B = LL2.add_node(node(element))
+
+position = 1
+
+while (A.value != B.value):
+	A = A.next
+	B = B.next
+	position += 1
+
+print(position)
+
+# the way sLL is setup ensure list are non cyclical
+# in python we can only assume nodes with same value are the exact same node obj
+# constant space is fullfilled as the search only takes O(2), 3 if counted the counter
+# linear (sum) time should be fullfilled assuming A and B have same length, the same value exists at the same position (eg. if 8 is changed to other value counter output will be 4), thus O(M+N) is satisfied where M+N = 2N
--- D21_solution.py
+++ D21_solution.py
@@ -2,3 +2,48 @@
 Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2
 """
+
+timetable = [(30, 75), (0, 50), (60, 150), (51,59)]
+
+bin_list = []
+
+ctr = 0
+
+def try_1(timetable, bin_list, ctr):
+	sorted_timetable = sorted(timetable)
+
+	for klass in sorted_timetable:
+		print(range(klass[0], klass[1]+1))
+		print(bin_list)
+		if (bool(set(range(klass[0], klass[1]+1)) & set(bin_list))) :
+			print(1)
+			bin_list.extend(range(klass[0], klass[1]+1))
+			ctr +=1
+		else:
+			print(2)
+			ext = list(set(range(klass[0], klass[1]+1)) - set(bin_list))
+			bin_list.extend(ext)
+
+	print(ctr)
+
+# this system is not robust, must have a release class room system1, could be a way out of it but not sure how
+
+def try_2(timetable):
+	start = sorted(list(t[0] for t in timetable))
+	end = sorted(list(t[1] for t in timetable))
+
+	max_room = room_occupied = 0
+	n = len(start)
+	i = j = 0
+
+	while i < n and j < n:
+		if start[i] < end[j]:
+			room_occupied += 1
+			max_room = max(max_room, room_occupied)
+			i += 1
+		else:
+			room_occupied -=1
+			j += 1
+	print(max_room)
+
+try_2(timetable)
