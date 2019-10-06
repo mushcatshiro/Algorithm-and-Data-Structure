bca8bec2eeb65648c54dc30cebda31cde1478357
shiro <yingjieho@hotmail.com>
2019-10-06 21:57:14 +0800

    clean up

--- algorithm_python/dfs.py
+++
@@ -1,7 +0,0 @@
-import queue as q
-import tree as t
-
-
-# create a tree prefably a perfect binary tree
-# traverse in dfs
-
--- algorithm_python/recursion.py
+++
@@ -1,7 +0,0 @@
-def rec (x):
-	if (x == 10):
-		return 10
-	else:
-		return (x + rec (x+1))
-
-print(rec(0))
