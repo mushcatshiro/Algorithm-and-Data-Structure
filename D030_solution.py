dde2e87c450aef5caf1e069aa5b603df5b12a294
shiro <yingjieho@hotmail.com>
2020-03-01 23:30:15 +0800

    update D31

--- D030_solution.py
+++ D030_solution.py
@@ -4,3 +4,21 @@ Compute how many units of water remain trapped on the map in O(N) time and O(1) 
 For example, given the input [2, 1, 2], we can hold 1 unit of water in the middle.
 Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water
 """
+
+l_1 = [2, 1, 2]
+l_2 = [3, 0, 1, 3, 0, 5]
+l_3 = [2, 3, 4, 1, 0, 8, 9, 6, 5]
+
+def waterVolume(l):
+	if len(l) > 2:
+		l = sorted(l, reverse=True)
+		f = l[0]
+		e = l[1]
+		remaining = l[2:]
+		vol = int(l[1]) * len(remaining)
+		for i in remaining:
+			vol = vol - i
+		print(vol)
+
+
+waterVolume(l_3)
--- D031_solution.py
+++ D031_solution.py
@@ -2,3 +2,14 @@
 The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other. For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
 Given two strings, compute the edit distance between them
 """
+
+s_1 = 'kitten'
+s_2 = 'sitting'
+
+def editDistance(s_1, s_2):
+	l_1 = list(s_1)
+	l_2 = list(s_2)
+
+	for i in l_1:
+		if i in l_2:
+			
